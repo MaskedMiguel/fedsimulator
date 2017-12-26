66d7b9f29acec93618d65f4a89cd1b3bd8791295
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-12-26 19:02:03 +0000

    Leaner segments component

--- src/components/segments/segments.js
+++ src/components/segments/segments.js
@@ -1,35 +1,25 @@
-import React, { PureComponent } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 
 import "./segments.scss"
 
-class Segments extends PureComponent {
-  render() {
-    return (
-      <div className="segments">
-        {this.props.segments.map((segment, key) => {
-          const { width, value, name, } = segment
-          const style = Object.assign({}, segment.style, { width: width + "%", })
-          return (
-            <span key={key} style={style} className="segment pulse">
-              <span className="name">{name}</span>
-              <span className="amount">{value.toLocaleString()}</span>
-            </span>
-          )
-        })}
-      </div>
-    )
-  }
-}
+const Segments = ({ segments = [], }) => (
+  <div className="segments">
+    {segments.map((segment, key) => {
+      const { width, value, name, } = segment
+      const style = Object.assign({}, segment.style, { width: width + "%", })
+      return (
+        <div key={key} style={style} className="segment pulse">
+          <span className="name">{name}</span>
+          <span className="amount">&nbsp;-&nbsp;{value.toLocaleString()}</span>
+        </div>
+      )
+    })}
+  </div>
+)
 
 Segments.propTypes = {
   segments: PropTypes.array,
 }
-
-Segments.defaultProps = {
-  segments: [],
-}
-
-Segments.displayName = "Segments"
 
 export default Segments
