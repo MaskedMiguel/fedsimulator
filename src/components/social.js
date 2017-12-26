5ee7c1830f889cc987fd1338fda6dbb32e82cf6b
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-12-26 19:01:14 +0000

    Fix social key issue

--- src/components/social.js
+++ src/components/social.js
@@ -1,15 +1,15 @@
 import React from "react"
 
-const Social = () => {
-  return [
-    <a target="_blank" rel="noopener" href="https://twitter.com/UniverseSimMan">
+const Social = () => (
+  <div>
+    <a target="_blank" rel="noopener" href="https://twitter.com/FedSimulator">
       <i className="fab fa-twitter" aria-hidden="true" /> Twitter
-    </a>,
-    ` `,
+    </a>
+    &nbsp;&nbsp;<i className="icon fa fa-ellipsis-v" aria-hidden="true" />&nbsp;&nbsp;
     <a target="_blank" rel="noopener" href="https://github.com/azz0r/fed-simulator">
       <i className="fab fa-github" aria-hidden="true" /> Github
-    </a>,
-  ]
-}
+    </a>
+  </div>
+)
 
 export default Social
