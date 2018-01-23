b678e091d14a390e8a1a76ab1ac9fbd039b138c5
Aaron Lote <aaron.lote@gamesys.co.uk>
2018-01-23 13:14:04 +0000

    random colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,14 +1,23 @@
 import chromatism from "chromatism"
+import arrayOfLength from "../helpers/array-of-length"
 
 export const color = "#be0f09"
-export const defaultColors = ["#000000", "#4A4A4A", "#9B9B9B", "#FFFFFF",]
+
+const randomHexColorCode = () => {
+  let n = ((Math.random() * 0xfffff) | 0).toString(16);
+  return '#' + (n.length !== 6 ? ((Math.random() * 0xf) | 0).toString(16) + n : n);
+};
 
 const degrees = 10
 
 const sections = 90
 
-const list = chromatism.adjacent(degrees, sections, color).hex
+export const defaultColors = arrayOfLength(20).map(item => randomHexColorCode())
 
-export const colors = Array.from(new Set(Object.assign([], list, defaultColors)))
+export const blandColors = ["#000000", "#4A4A4A", "#9B9B9B", "#FFFFFF",]
+
+export const list = chromatism.adjacent(degrees, sections, color).hex
+
+export const colors = Array.from(new Set(Object.assign([], list, defaultColors, blandColors)))
 
 export const SHADE = -60
