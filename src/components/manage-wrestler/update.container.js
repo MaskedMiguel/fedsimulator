0265292d1014af58d4acd8194b6e298b42f48bf9
Aaron Lote <aaron.lote@gamesys.co.uk>
2018-03-14 11:28:56 +0000

    Reselecting a championship will unselect it

--- src/components/manage-wrestler/update.container.js
+++ src/components/manage-wrestler/update.container.js
@@ -1,5 +1,6 @@
 import { compose, withProps } from "recompose"
 
+import withRoster from "../../hoc/withRoster"
 import withBrands from "../../hoc/withBrands"
 import withChampionships from "../../hoc/withChampionships"
 import withStyle from "../../hoc/withHighlightedStyle"
@@ -9,15 +10,22 @@ import { updateWrestler, deleteWrestler } from "../../actions/roster"
 
 import EditWrestler from "./wrestler"
 
-const propsMapper = ({ dispatch, id, }) => ({
-  onResetChampionship: () => dispatch(updateWrestler({ championshipId: null, id: id, })),
-  onChampionshipSelected: championshipId => dispatch(updateWrestler({ championshipId: String(championshipId), id: id, })),
-  onGenderUpdate: male => dispatch(updateWrestler({ male: Boolean(male), id: id, })),
-  onBrandSelected: brandId => dispatch(updateWrestler({ championshipId: null, brandId: String(brandId), id: id, })),
-  onPointsUpdate: e => dispatch(updateWrestler({ points: Number(e.target.value), id: id, })),
-  onNameUpdate: e => dispatch(updateWrestler({ name: String(e.target.value), id: id, })),
-  onImageUpdate: (name, value) => dispatch(updateWrestler({ image: String(value), id: id, })),
-  onResetImage: () => dispatch(updateWrestler({ image: "", id: id, })),
+const propsMapper = ({ dispatch, roster, id }) => ({
+  onResetChampionship: () => dispatch(updateWrestler({ championshipId: null, id: id })),
+  onChampionshipSelected: championshipId => {
+    const wrestler = roster.find(item => item.id === id)
+
+    if (wrestler.championshipId === championshipId) {
+      championshipId = null
+    }
+    return dispatch(updateWrestler({ championshipId: String(championshipId), id }))
+  },
+  onGenderUpdate: male => dispatch(updateWrestler({ male: Boolean(male), id: id })),
+  onBrandSelected: brandId => dispatch(updateWrestler({ championshipId: null, brandId: String(brandId), id: id })),
+  onPointsUpdate: e => dispatch(updateWrestler({ points: Number(e.target.value), id: id })),
+  onNameUpdate: e => dispatch(updateWrestler({ name: String(e.target.value), id: id })),
+  onImageUpdate: (name, value) => dispatch(updateWrestler({ image: String(value), id: id })),
+  onResetImage: () => dispatch(updateWrestler({ image: null, id: id })),
   onDelete: () => {
     if (confirm(WRESTLER_CONFIRM_DELETE)) {
       dispatch(deleteWrestler(id))
@@ -27,6 +35,7 @@ const propsMapper = ({ dispatch, id, }) => ({
 
 export default compose(
   withChampionships,
+  withRoster,
   withBrands,
   withStyle,
   withProps(propsMapper) //
